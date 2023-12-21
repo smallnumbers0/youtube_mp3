c6b0e90013ee1a27c3c35d0904d42fe52fdbcb0c
Jacky <jchoi1459@gmail.com>
2023-12-20 21:53:19 -0800

    update

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post("/convert-mp3", async (req, res)=> {
         const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, { 
             "method" : "GET",
             "headers" : {
-                "x-rapidapi-key" : "4c73d59859msh79fd72bada95635p157af7jsnb98e32829c48",
+                "x-rapidapi-key" : "API_KEY",
                 "x-rapidapi-host" : "youtube-mp36.p.rapidapi.com"
             }
         });
